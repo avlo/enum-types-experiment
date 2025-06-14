665588a984f189b639bfce91d29ae5f81cf585a9
nick avlo <avlo@yahoo.com>
2025-06-14 00:49:26 -0700

    ongoing

--- src/main/java/com/prosilion/nostr/event/PriceTag.java
+++ src/main/java/com/prosilion/nostr/event/PriceTag.java
@@ -1,82 +1,42 @@
-//package com.prosilion.nostr.event;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-//import com.fasterxml.jackson.databind.JsonNode;
-//import java.math.BigDecimal;
-//import java.util.Objects;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NonNull;
-//import lombok.RequiredArgsConstructor;
-//import nostr.base.annotation.Key;
-//import nostr.base.annotation.Tag;
-//import nostr.event.BaseTag;
-//
-//@Builder
-//@Data
-//@Tag(code = "price", nip = 99)
-//@RequiredArgsConstructor
-//@AllArgsConstructor
-//@JsonPropertyOrder({"number", "currency", "frequency"})
-//public class PriceTag extends BaseTag {
-//
-//    @Key
-//    @JsonProperty
-//    @JsonFormat(shape = JsonFormat.Shape.STRING)
-//    private BigDecimal number;
-//
-//    @Key
-//    @JsonProperty
-//    private String currency;
-//
-//    @Key
-//    @JsonProperty
-//    private String frequency;
-//
-//    public static <T extends BaseTag> T deserialize(@NonNull JsonNode node) {
-//        PriceTag tag = new PriceTag();
-//        setRequiredField(node.get(1), (n, t) -> tag.setNumber(new BigDecimal(n.asText())), tag);
-//        setOptionalField(node.get(2), (n, t) -> tag.setCurrency(n.asText()), tag);
-//        setOptionalField(node.get(3), (n, t) -> tag.setFrequency(n.asText()), tag);
-//        return (T) tag;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (o == null || getClass() != o.getClass()) return false;
-//        if (!super.equals(o)) return false;
-//        PriceTag priceTag = (PriceTag) o;
-//        return Objects.equals(
-//                number.stripTrailingZeros(),
-//                priceTag.number.stripTrailingZeros()
-//        )
-//               && Objects.equals(currency, priceTag.currency) && Objects.equals(frequency, priceTag.frequency);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(super.hashCode(), number.stripTrailingZeros(), currency, frequency);
-//    }
-//
-//    public static PriceTag updateFields(@NonNull GenericTag genericTag) {
-//        if (!"price".equals(genericTag.getCode())) {
-//            throw new IllegalArgumentException("Invalid tag code for PriceTag");
-//        }
-//
-//        if (genericTag.getAttributes().size() < 2 || genericTag.getAttributes().size() > 3) {
-//            throw new IllegalArgumentException("Invalid number of attributes for PriceTag");
-//        }
-//
-//        PriceTag tag = new PriceTag();
-//        tag.setNumber(new BigDecimal(genericTag.getAttributes().get(0).getValue().toString()));
-//        tag.setCurrency(genericTag.getAttributes().get(1).getValue().toString());
-//
-//        if (genericTag.getAttributes().size() > 2) {
-//            tag.setFrequency(genericTag.getAttributes().get(2).getValue().toString());
-//        }
-//        return tag;
-//    }
-//}
+package com.prosilion.nostr.event;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.lang.NonNull;
+
+@Tag(code = "price", nip = 99)
+@JsonPropertyOrder({"number", "currency", "frequency"})
+public record PriceTag(
+    @Key @JsonProperty @JsonFormat(shape = JsonFormat.Shape.STRING) BigDecimal number,
+    @Key @JsonProperty String currency,
+    @Key @JsonProperty String frequency) implements BaseTag {
+
+  public BaseTag deserialize(@NonNull JsonNode node) {
+    return new PriceTag(
+        new BigDecimal(Optional.of(node.get(1)).orElseThrow().asText()),
+        Optional.of(node.get(1)).orElseThrow().asText(),
+        Optional.of(node.get(2)).orElseThrow().asText()
+    );
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    PriceTag priceTag = (PriceTag) o;
+    return Objects.equals(
+        number.stripTrailingZeros(),
+        priceTag.number.stripTrailingZeros()) 
+      && 
+        Objects.equals(currency, priceTag.currency) && Objects.equals(frequency, priceTag.frequency);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(number.stripTrailingZeros(), currency, frequency);
+  }
+}
